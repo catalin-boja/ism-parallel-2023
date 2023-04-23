1e8cdf2174ede49d0c6eceac1271ee51a7d2e30e
Catalin Boja <catalin.boja@ie.ase.ro>
2023-04-23 13:46:01 +0300

    Update example v2

--- OpenMPComplexDirectives/Source.cpp
+++ OpenMPComplexDirectives/Source.cpp
@@ -9,6 +9,62 @@ bool isPrime(long value) {
 	return true;
 }
 
+int standardSolution(const int N) {
+	int counter = 0;
+
+	//standard solution  to count number of primes from 1 to N
+	for (long i = 1; i < N; i++) {
+		if (isPrime(i)) {
+			counter += 1;
+		}
+	}
+
+	return counter;
+}
+
+//version 1 with OpenMP
+
+//counter is shared netween all threads -> racing conditions
+//thre for loop is copy-pasted in all threads -> all threads process the same interval
+int ompSolutionV1(const int N) {
+	int counter = 0;
+
+#pragma omp parallel 
+	{
+		for (long i = 1; i < N; i++) {
+			if (isPrime(i)) {
+				counter += 1;
+			}
+		}
+	}
+
+	return counter;
+}
+
+//version 2 with OpenMP
+
+int ompSolutionV2(const int N) {
+	int counter = 0;
+
+#pragma omp parallel 
+	{
+		int size = N / omp_get_num_threads();
+		long lowerLimit = omp_get_thread_num() * size;
+		long upperLimit = 
+			(omp_get_thread_num() == omp_get_num_threads() - 1) ? 
+				N : (omp_get_thread_num() + 1) * size;
+
+		for (long i = lowerLimit; i < upperLimit; i++) {
+			if (isPrime(i)) {
+				counter += 1;
+			}
+		}
+
+	}
+
+	return counter;
+}
+
 int main() {
 
 	const int N = 4e5;
@@ -18,12 +74,7 @@ int main() {
 
 	double tStart = omp_get_wtime();
 
-	//standard solution  to count number of primes from 1 to N
-	for (long i = 1; i <= N; i++) {
-		if (isPrime(i)) {
-			counter += 1;
-		}
-	}
+	counter = ompSolutionV2(N);
 
 	double tFinal = omp_get_wtime();
 
